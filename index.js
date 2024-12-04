84593ce74653d0ef819835da6966c9c38de239d2
sameerkun-sureify <surya.kuntamukkala@sureify.com>
2024-12-05 01:53:07 +0530

    Added code to pass test cases of single number and multiple numbers

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function add(nums)
     {
         return 0;
     }
+    nums=nums.split(",").map((num)=>{
+        return parseInt(num,10);
+    });
+    return nums.reduce((acc,curr)=>{
+        return acc+curr;
+    },0);
 }
 
 module.exports = add;
