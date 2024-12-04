0ba5d9fdcfe06531a7b96274e2a802280b6bb801
sameerkun-sureify <surya.kuntamukkala@sureify.com>
2024-12-05 02:28:24 +0530

    Added code to throw error when negative numbers are passed

--- index.js
+++ index.js
@@ -7,17 +7,21 @@ function add(nums)
 
     let delimiter = /,|\n|;/;
     if (nums.startsWith("//")) {
-        const parts = nums.split("\n");
+        const parts = nums.split("\n"); 
         delimiter = new RegExp(parts[0].slice(2));
         nums = parts[1];
     }
-
     if (/^\d+[\n,;]$/.test(nums)) {
         throw new Error("Invalid input: single number with delimiter");
     }
     nums=nums.split(delimiter).map((num)=>{
         return parseInt(num,10);
     });
+    const negatives = nums.filter((num) => num < 0);
+
+    if (negatives.length > 0) {
+        throw new Error(`negatives not allowed: ${negatives.join(", ")}`);
+    }
     return nums.reduce((acc,curr)=>{
         return acc+curr;
     },0);
