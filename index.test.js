bb3ed250640689aea4a824cd5f2041388652a97a
sameerkun-sureify <surya.kuntamukkala@sureify.com>
2024-12-05 01:48:07 +0530

    Added test cases to handle inputs of single number and multiple numbers

--- index.test.js
+++ index.test.js
@@ -4,4 +4,12 @@ describe('add function', () => {
     it('should return 0 when empty string is passed', () => {
         expect(add("")).toBe(0);
     });
+    it('should return the number itself when a single number is passed', () => {
+        expect(add("1")).toBe(1);
+        expect(add("5")).toBe(5);
+    });
+    it('should return the sum of two numbers when two numbers are passed', () => {
+        expect(add("1,2")).toBe(3);
+        expect(add("10,20")).toBe(30);
+    });
 });
