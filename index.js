9c0401369b4c85fd4b9ff1c82e25f027d03cf456
sameerkun-sureify <surya.kuntamukkala@sureify.com>
2024-12-06 23:54:56 +0530

    Refactored Prod code

--- index.js
+++ index.js
@@ -8,30 +8,42 @@ function extracteDelimiter(delimiterPart) {
         return new RegExp(delimiterPart.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"));
     }
 }
-function add(nums) {
-    if (nums === "") {
-        return 0;
 
-    }
-
-    let delimiter = /[,\n;*#]/;
-    if (nums.startsWith("//")) {
-        const parts = nums.split("\n");
-        const delimiterPart = parts[0].slice(2);
-        delimiter = extracteDelimiter(delimiterPart);
-        nums = parts[1];
-    }
-    if (/^\d+[\n,;*#]$/.test(nums)) {
-        throw new Error("Invalid input: single number with delimiter");
-    }
-    nums = nums.split(delimiter).map((num) => {
-        return num <= 1000 ? parseInt(num, 10) : 0;
-    });
+function checkForNegatives(nums) {
     const negatives = nums.filter((num) => num < 0);
 
     if (negatives.length > 0) {
         throw new Error(`negatives not allowed: ${negatives.join(", ")}`);
     }
+}
+
+function parsedNums(nums, delimiter) {
+    return nums.split(delimiter).map((num) => {
+        return parseInt(num);
+    }).filter((num)=>{
+        return  num<=1000
+    });
+}
+
+function add(nums) {
+    if (nums === "") {
+        return 0;
+
+    }
+    let delimiter = /[,\n;*#]/;
+    const hasCustomDelimiter = nums.startsWith("//");
+    if (hasCustomDelimiter) {
+        const parts = nums.split("\n");
+        const delimiterPart = parts[0].slice(2);
+        delimiter = extracteDelimiter(delimiterPart);
+        nums = parts[1];
+    }
+    const checkSingleNumberWithDelimiter=/^\d+[\n,;*#]$/
+    if (checkSingleNumberWithDelimiter.test(nums)) {
+        throw new Error("Invalid input: single number with delimiter");
+    }
+    nums = parsedNums(nums, delimiter);
+    checkForNegatives(nums);
     return nums.reduce((acc, curr) => {
         return acc + curr;
     }, 0);
