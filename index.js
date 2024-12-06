9c96b90237db7034913a48cc90f857279f551703
sameerkun-sureify <surya.kuntamukkala@sureify.com>
2024-12-06 15:06:37 +0530

    Formatted code using prettier

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
-function add(nums)
-{
-    if(nums == "")
-    {
-        return 0;
-    }
+function add(nums) {
+  if (nums == "") {
+    return 0;
+  }
 
-    let delimiter = /,|\n|;|\*|#/;
-    if (nums.startsWith("//")) {
-        const parts = nums.split("\n");
-        const dlPart = parts[0].slice(2);
-        if (dlPart.startsWith("[") && dlPart.endsWith("]")) {
-            console.log(dlPart.match(/\[([^\]]+)\]/g));
-            const dls = dlPart.match(/\[([^\]]+)\]/g).map(d => d.slice(1, -1));
-            delimiter = new RegExp(dls.map(d => d.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')).join("|"));
-        } else {
-            delimiter = new RegExp(dlPart.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'));
-        }
-        nums = parts[1];
+  let delimiter = /,|\n|;|\*|#/;
+  if (nums.startsWith("//")) {
+    const parts = nums.split("\n");
+    const dlPart = parts[0].slice(2);
+    if (dlPart.startsWith("[") && dlPart.endsWith("]")) {
+      const dls = dlPart.match(/\[([^\]]+)\]/g).map((d) => d.slice(1, -1));
+      delimiter = new RegExp(
+        dls.map((d) => d.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&")).join("|"),
+      );
+    } else {
+      delimiter = new RegExp(dlPart.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"));
     }
-    if (/^\d+[\n,;*#]$/.test(nums)) {
-        throw new Error("Invalid input: single number with delimiter");
-    }
-    nums=nums.split(delimiter).map((num)=>{
-        return num<=1000?parseInt(num,10) : 0;
-    });
-    const negatives = nums.filter((num) => num < 0);
+    nums = parts[1];
+  }
+  if (/^\d+[\n,;*#]$/.test(nums)) {
+    throw new Error("Invalid input: single number with delimiter");
+  }
+  nums = nums.split(delimiter).map((num) => {
+    return num <= 1000 ? parseInt(num, 10) : 0;
+  });
+  const negatives = nums.filter((num) => num < 0);
 
-    if (negatives.length > 0) {
-        throw new Error(`negatives not allowed: ${negatives.join(", ")}`);
-    }
-    return nums.reduce((acc,curr)=>{
-        return acc+curr;
-    },0);
+  if (negatives.length > 0) {
+    throw new Error(`negatives not allowed: ${negatives.join(", ")}`);
+  }
+  return nums.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0);
 }
 
 module.exports = add;
--- index.test.js
+++ index.test.js
@@ -1,49 +1,54 @@
-const add = require('./index');
+const add = require("./index");
 
-describe('String Calculator tests', () => {
-    it('should return 0 when empty string is passed', () => {
-        expect(add("")).toBe(0);
-    });
-    it('should return the number itself when a single number is passed', () => {
-        expect(add("1")).toBe(1);
-        expect(add("5")).toBe(5);
-    });
-    it('should return the sum of two numbers when two numbers are passed', () => {
-        expect(add("1,2")).toBe(3);
-        expect(add("10,20")).toBe(30);
-    });
-    it('should throw an error if a single number is passed with delimiter', () => {
-        expect(() => add("1\n")).toThrow("Invalid input: single number with delimiter");
-        expect(() => add("10,")).toThrow("Invalid input: single number with delimiter");
-    });
-    it('should return the sum of two numbers when two numbers are passed with a newline delimiter', () => {
-        expect(add("1\n2")).toBe(3);
-        expect(add("10\n20")).toBe(30);
-    });
-    it('should return the sum of two numbers when two numbers are passed with a custom delimiter', () => {
-        expect(add("//;\n1;2")).toBe(3);
-        expect(add("//;\n10;20")).toBe(30);
-    });
-    it('should throw an error when negative numbers are passed', () => {
-        expect(() => add("1,-2,3")).toThrow("negatives not allowed: -2");
-        expect(() => add("//;\n1;-2;3;-4")).toThrow("negatives not allowed: -2, -4");
-    });
-    it('should just skip when numbers greater than 1000 are passed', () => {
-        expect( add("1001")).toBe(0);
-        expect(add("1001,3")).toBe(3);
-        expect(add("1001,10004")).toBe(0);
-    });
-    it('should return the sum of numbers with custom delimiter of any length', () => {
-        expect(add("//[***]\n1***2***3")).toBe(6);
-        expect(add("//[###]\n4###5###6")).toBe(15);
-    });
-    it('should return the sum of numbers with multiple custom delimiters', () => {
-        expect(add("//[*][%]\n1*2%3")).toBe(6);
-        expect(add("//[;][#]\n4;5#6")).toBe(15);
-    });
-    it('should return the sum of numbers with multiple custom delimiters of any length', () => {
-        expect(add("//[***][%%%]\n1***2%%%3")).toBe(6);
-        expect(add("//[;;][##]\n4;;5##6")).toBe(15);
-    });
+describe("String Calculator tests", () => {
+  it("should return 0 when empty string is passed", () => {
+    expect(add("")).toBe(0);
+  });
+  it("should return the number itself when a single number is passed", () => {
+    expect(add("1")).toBe(1);
+    expect(add("5")).toBe(5);
+  });
+  it("should return the sum of two numbers when two numbers are passed", () => {
+    expect(add("1,2")).toBe(3);
+    expect(add("10,20")).toBe(30);
+  });
+  it("should throw an error if a single number is passed with delimiter", () => {
+    expect(() => add("1\n")).toThrow(
+      "Invalid input: single number with delimiter",
+    );
+    expect(() => add("10,")).toThrow(
+      "Invalid input: single number with delimiter",
+    );
+  });
+  it("should return the sum of two numbers when two numbers are passed with a newline delimiter", () => {
+    expect(add("1\n2")).toBe(3);
+    expect(add("10\n20")).toBe(30);
+  });
+  it("should return the sum of two numbers when two numbers are passed with a custom delimiter", () => {
+    expect(add("//;\n1;2")).toBe(3);
+    expect(add("//;\n10;20")).toBe(30);
+  });
+  it("should throw an error when negative numbers are passed", () => {
+    expect(() => add("1,-2,3")).toThrow("negatives not allowed: -2");
+    expect(() => add("//;\n1;-2;3;-4")).toThrow(
+      "negatives not allowed: -2, -4",
+    );
+  });
+  it("should just skip when numbers greater than 1000 are passed", () => {
+    expect(add("1001")).toBe(0);
+    expect(add("1001,3")).toBe(3);
+    expect(add("1001,10004")).toBe(0);
+  });
+  it("should return the sum of numbers with custom delimiter of any length", () => {
+    expect(add("//[***]\n1***2***3")).toBe(6);
+    expect(add("//[###]\n4###5###6")).toBe(15);
+  });
+  it("should return the sum of numbers with multiple custom delimiters", () => {
+    expect(add("//[*][%]\n1*2%3")).toBe(6);
+    expect(add("//[;][#]\n4;5#6")).toBe(15);
+  });
+  it("should return the sum of numbers with multiple custom delimiters of any length", () => {
+    expect(add("//[***][%%%]\n1***2%%%3")).toBe(6);
+    expect(add("//[;;][##]\n4;;5##6")).toBe(15);
+  });
 });
-
