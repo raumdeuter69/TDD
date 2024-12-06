2c0fb1a22eb0594565cc22f787d6f5d58266f192
sameerkun-sureify <surya.kuntamukkala@sureify.com>
2024-12-06 15:37:02 +0530

    Refactored Prod code

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
-function add(nums) {
-  if (nums == "") {
-    return 0;
-  }
+function extracteDelimiter(delimiterPart) {
+    if (delimiterPart.startsWith("[") && delimiterPart.endsWith("]")) {
+        const extractedDelimiter = delimiterPart.match(/\[([^\]]+)\]/g).map((d) => d.slice(1, -1));
+        return new RegExp(
+            extractedDelimiter.map((d) => d.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&")).join("|"))
 
-  let delimiter = /,|\n|;|\*|#/;
-  if (nums.startsWith("//")) {
-    const parts = nums.split("\n");
-    const dlPart = parts[0].slice(2);
-    if (dlPart.startsWith("[") && dlPart.endsWith("]")) {
-      const dls = dlPart.match(/\[([^\]]+)\]/g).map((d) => d.slice(1, -1));
-      delimiter = new RegExp(
-        dls.map((d) => d.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&")).join("|"),
-      );
     } else {
-      delimiter = new RegExp(dlPart.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"));
+        return new RegExp(delimiterPart.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&"));
     }
-    nums = parts[1];
-  }
-  if (/^\d+[\n,;*#]$/.test(nums)) {
-    throw new Error("Invalid input: single number with delimiter");
-  }
-  nums = nums.split(delimiter).map((num) => {
-    return num <= 1000 ? parseInt(num, 10) : 0;
-  });
-  const negatives = nums.filter((num) => num < 0);
+}
+function add(nums) {
+    if (nums === "") {
+        return 0;
 
-  if (negatives.length > 0) {
-    throw new Error(`negatives not allowed: ${negatives.join(", ")}`);
-  }
-  return nums.reduce((acc, curr) => {
-    return acc + curr;
-  }, 0);
+    }
+
+    let delimiter = /[,\n;*#]/;
+    if (nums.startsWith("//")) {
+        const parts = nums.split("\n");
+        const delimiterPart = parts[0].slice(2);
+        delimiter = extracteDelimiter(delimiterPart);
+        nums = parts[1];
+    }
+    if (/^\d+[\n,;*#]$/.test(nums)) {
+        throw new Error("Invalid input: single number with delimiter");
+    }
+    nums = nums.split(delimiter).map((num) => {
+        return num <= 1000 ? parseInt(num, 10) : 0;
+    });
+    const negatives = nums.filter((num) => num < 0);
+
+    if (negatives.length > 0) {
+        throw new Error(`negatives not allowed: ${negatives.join(", ")}`);
+    }
+    return nums.reduce((acc, curr) => {
+        return acc + curr;
+    }, 0);
 }
 
 module.exports = add;
